e524588de82fbec16b034ebe046e11f216b3eb02
Kevin Booth <kdbooth97@gmail.com>
2019-07-08 23:11:29 -0400

    Add photo and styles

--- src/components/Glowbar.vue
+++ src/components/Glowbar.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="glowbar">
-
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="profile">
+      <img src="@/assets/kevin.jpg" />
+    </div>
     <HelloWorld msg="kevin booth"/>
   </div>
 </template>
@@ -15,3 +18,16 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.profile {
+  padding: 2rem 0;
+
+  img {
+    max-width: 14rem;
+    border-radius: 100%;
+    border: 3px solid #80ffc6;
+  }
+}
+</style>
+
