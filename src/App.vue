246b4e391635158491cee8ff1ec45f4920c06888
Kevin Booth <kdbooth97@gmail.com>
2019-07-08 22:28:53 -0400

    Update homepage styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <glowbar></glowbar>
     <div id="nav">
       <router-link to="/">home</router-link> |
       <router-link to="/about">about</router-link>
@@ -8,15 +9,48 @@
   </div>
 </template>
 
+<script>
+import Glowbar from '@/components/Glowbar.vue'
+
+export default {
+  components: {
+    Glowbar
+  }
+}
+</script>
+
+
 <style>
 body {
   background-color: #031329;
   color: #80ffc6 !important;
   font-family: monospace !important;
+	background: linear-gradient(-45deg, #031329, #0f061d, #003244, #0b4033);
+	background-size: 400% 400%;
+	-webkit-animation: body-animate 15s infinite;
+	-moz-animation: body-animate 15s infinite;
+	animation: body-animate 15s infinite;
+}
+
+@-webkit-keyframes body-animate {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+
+@-moz-keyframes body-animate {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+
+@keyframes body-animate {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
 }
 
 #app {
-  padding-top: 1rem;
   text-align: center;
 }
 
---
+++ src/components/Glowbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="glowbar">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+.glowbar {
+  height: 1rem;
+  background: linear-gradient(90deg, #e167ff, #00694b);
+  margin-bottom: 2rem;
+}
+</style>
