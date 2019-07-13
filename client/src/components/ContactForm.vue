5115f015a57304ab17aed0f62346b09b5b7acba2
Kevin Booth <kdbooth97@gmail.com>
2019-07-13 15:26:54 -0400

    Add form styles and update home view

---
+++ client/src/components/ContactForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form>
+      <input placeholder="Name" type="text">
+      <input placeholder="Email" type="email">
+      <input placeholder="Subject" type="text">
+      <textarea placeholder="Message" type="text"></textarea>
+      <button class="button">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -11,23 +11,37 @@
         :erase-delay=50
       ></vue-typer>
     </div>
+    <contact-form v-bind:class="{ visible: !showForm }"></contact-form>
+    <button 
+      v-show="!showForm" 
+      v-on:click="showForm = !showForm" 
+      class="button--secondary"
+    >Contact Me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { VueTyper } from 'vue-typer'
+import ContactForm from '@/components/ContactForm.vue'
 
 export default {
   name: 'home',
   components: {
-    VueTyper
+    VueTyper,
+    ContactForm
   },
   data() {
     return {
-      typer: ["Hi, my name's Kevin Booth!", "And I'm a fullstack web developer!"]
+      typer: ["Hi, my name's Kevin Booth!", "And I'm a fullstack web developer!"],
+      showForm: false
     }
   },
 }
 </script>
 
+<style lang="scss">
+.visible {
+  visibility: hidden;
+}
+</style>
