88984a813e6176383b28a1c1de453e70d7615734
Kevin Booth <kdbooth97@gmail.com>
2019-07-13 16:33:26 -0400

    Update contact form component

--- client/src/components/ContactForm.vue
+++ client/src/components/ContactForm.vue
@@ -2,10 +2,10 @@
   <div class="form__container form__container--contact">
     <span class="form__title">Contact Me</span>
     <div class="form">
-      <input placeholder="Name" type="text">
-      <input placeholder="Email" type="email">
-      <input placeholder="Subject" type="text">
-      <textarea placeholder="Message" type="text"></textarea>
+      <input placeholder="Name" type="text" required>
+      <input placeholder="Email" type="email" required>
+      <input placeholder="Subject" type="text" required>
+      <textarea placeholder="Message" type="text" required></textarea>
       <button class="button">Submit</button>
     </div>
   </div>
