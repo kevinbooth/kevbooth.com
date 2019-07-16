c9bdd4f93028c7acf6cf3ddf1008c7bf39312acf
Kevin Booth <kdbooth97@gmail.com>
2019-07-15 23:25:49 -0400

    Update logo and route it to homepage

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
       <div class="nav unicon">
-        <img src="@/assets/logo.svg" />
+        <router-link to="/"><img src="@/assets/logo.svg" /></router-link>
         <router-link to="/"><unicon name="home-alt" width="30" height="30"></unicon></router-link>
         <router-link to="/skills"><unicon name="screw" width="30" height="30"></unicon></router-link>
       </div>
